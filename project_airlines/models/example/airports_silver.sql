1acb664a8b8d0fe2fabe3fd455fd88f90ef46c5e
Ashutosh848 <ashu.tiwari.9741@gmail.com>
2025-07-15 21:15:43 +0530

    Incremental Loads(Initial) for all the tables

---
+++ project_airlines/models/example/airports_silver.sql
@@ -0,0 +1,12 @@
+-- models/airports_cleaned.sql
+{{ config(schema='silver', materialized='incremental') }}
+
+
+SELECT *
+FROM {{ source('bronze', 'airports_raw') }}
+{% if is_incremental() %}
+  WHERE updated_at > (
+    SELECT max(updated_at)
+    FROM {{ this }}
+  )
+{% endif %}
--- project_airlines/models/example/customers_silver.sql
+++ project_airlines/models/example/customers_silver.sql
@@ -1,9 +1,9 @@
 -- models/airports_cleaned.sql
-{{ config(schema='bronze', materialized='incremental') }}
+{{ config(schema='silver', materialized='incremental') }}
 
 
 SELECT *
-FROM {{ source('bronze', 'bookings_raw') }}
+FROM {{ source('bronze', 'customers_raw') }}
 {% if is_incremental() %}
   WHERE updated_at > (
     SELECT max(updated_at)
